01f5875c4440d99a36191356a52870458d025872
Eli Spencer <elispencer47@gmail.com>
2020-11-27 18:48:14 -0800

    Started Project

---
+++ ScheduleBrewSolution/ScheduleBrewEFClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScheduleBrewEFClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
