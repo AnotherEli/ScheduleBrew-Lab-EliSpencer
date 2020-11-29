7d308ee5d340494b23616ef7c78d241b381742d3
Eli Spencer <elispencer47@gmail.com>
2020-11-29 14:00:23 -0800

    Began Testing

--- ScheduleBrewSolution/ScheduleBrewProjectTests/ScheduleBrewTests.cs
+++ ScheduleBrewSolution/ScheduleBrewProjectTests/ScheduleBrewTests.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+using NUnit.Framework;
+using ScheduleBrewEFClasses.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ScheduleBrewProjectTests
 {
-    class ScheduleBrewTests
+    [TestFixture]
+    public class ScheduleBrewTests
     {
+        //Recipe Tests
+            //Recipe_Ingredient Tests
+            //Style Tests
+        //Ingredient Tests
+            //Ingredient_Inventory_Addition Tests 
+            //Ingredient_Inventory_Subtraction Tests
+            //Ingredient_Substitute Tests
+            
+        //Batch Tests
+            //Batch_Container Tests
+            //Brew_Container Tests
 
     }
 }
