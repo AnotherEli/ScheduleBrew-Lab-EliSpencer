34b4fb2f34864c19a185c99154a38a718c930f4f
Eli Spencer <elispencer47@gmail.com>
2020-11-29 12:06:14 -0800

    Finished Implementing EFCore

---
+++ ScheduleBrewSolution/ScheduleBrewEFClasses/Models/Adjunct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScheduleBrewEFClasses.Models
+{
+    public partial class Adjunct
+    {
+        public int IngredientId { get; set; }
+        public int AdjunctTypeId { get; set; }
+        public string UseFor { get; set; }
+        public double? RecommendedQuantity { get; set; }
+        public double? BatchVolume { get; set; }
+        public int? RecommendedUseDuringId { get; set; }
+
+        public virtual AdjunctType AdjunctType { get; set; }
+        public virtual Ingredient Ingredient { get; set; }
+        public virtual UseDuring RecommendedUseDuring { get; set; }
+
+        public override string ToString()
+        {
+            return "ID: " + IngredientId + ", Adjunct Type ID: " + AdjunctTypeId + "Use For: " + UseFor + "Recommended Quantity: " + RecommendedQuantity + " Batch Volume: " + BatchVolume + "Recommended Use During ID: " + RecommendedUseDuringId;
+        }
+    }
+}
--- ScheduleBrewSolution/ScheduleBrewEFClasses/Program.cs
+++ ScheduleBrewSolution/ScheduleBrewEFClasses/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace ScheduleBrewEFClasses
+namespace ScheduleBrewProjectTests
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
